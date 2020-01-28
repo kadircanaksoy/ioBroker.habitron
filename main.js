7cdb128c2abd54d719851c62d01482145e874c13
e2236883 <e2236883@ceng.metu.edu.tr>
2020-01-28 16:45:57 +0300

    testorino

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ class Template extends utils.Adapter {
     onStateChange(id, state) {
         if (state) {
             // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+            this.log.info(`daddy, the state ${id} changed: ${state.val} (ack = ${state.ack})`);
         } else {
             // The state was deleted
             this.log.info(`state ${id} deleted`);
